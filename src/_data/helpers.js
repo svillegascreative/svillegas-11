a27880d323895828778a40e45d8cd91b38ed25a1
sylvia villegas <sylvia@svillegas.com>
2018-12-14 22:30:04 -0500

    Create a siteUrl variable depending on env't

--- src/_data/helpers.js
+++ src/_data/helpers.js
@@ -1,4 +1,5 @@
 module.exports = {
+	siteUrl: process.env.ELEVENTY_ENV === 'development' ? 'http://localhost:3000' : 'https://svillegas.com',
 
 	niceDate: function (date) {
 		return date.toLocaleString("en-CA", { month: 'short', day: 'numeric', year: 'numeric'})
